ba9116da6b89e55df1f2cd7146d10b3ad07cc19f
hankcs <jfservice@126.com>
2020-12-22 15:46:36 -0500

    A better electra model

--- elit/pretrained/mtl.py
+++ elit/pretrained/mtl.py
@@ -18,7 +18,7 @@
 # Author: hankcs
 from elit.common.constant import ELIT_URL
 
-LEM_POS_NER_DEP_SDP_CON_AMR_ELECTRA_BASE_EN = ELIT_URL + 'en_lem_pos_ner_srl_dep_con_amr_electra_base_20201218.zip'
+LEM_POS_NER_DEP_SDP_CON_AMR_ELECTRA_BASE_EN = ELIT_URL + 'en_pos_ner_srl_dep_con_amr_electra_base_20201222.zip'
 LEM_POS_NER_DEP_SDP_CON_AMR_ROBERTA_BASE_EN = ELIT_URL + 'en_pos_ner_srl_dep_con_amr_roberta_base_20201219.zip'
 
 ALL = {}
