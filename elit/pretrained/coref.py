41f4c0289f56d1995b88efd311b04372b7d7abc2
lxucs <leonxu624@gmail.com>
2020-12-22 20:05:02 -0500

    Added coref loading and files

---
+++ elit/pretrained/coref.py
@@ -0,0 +1,26 @@
+# ========================================================================
+# Copyright 2020 Emory University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+
+# -*- coding:utf-8 -*-
+# Author: Liyan Xu
+from elit.common.constant import ELIT_URL
+
+DOC_COREF_SPANBERT_BASE_EN = ELIT_URL + 'doc_coref_spanbert_base_20201222.zip'
+DOC_COREF_SPANBERT_LARGE_EN = ELIT_URL + 'doc_coref_spanbert_large_20201222.zip'
+ONLINE_COREF_SPANBERT_BASE_EN = ELIT_URL + 'online_coref_spanbert_base_20201222.zip'
+ONLINE_COREF_SPANBERT_LARGE_EN = ELIT_URL + 'online_coref_spanbert_large_20201222.zip'
+
+ALL = {}
