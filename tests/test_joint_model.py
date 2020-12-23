86478c9df7b38cd73cdc83c08bf6172605fa8e96
lxucs <leonxu624@gmail.com>
2020-12-23 12:23:52 -0500

    Added coref test

--- tests/test_joint_model.py
+++ tests/test_joint_model.py
@@ -2,7 +2,7 @@
 # Author: hankcs
 # Date: 2020-12-18 19:21
 import unittest
-from elit.server.en import en
+from elit.server.en import en_services
 from elit.server.format import Input
 
 
@@ -14,17 +14,15 @@ class TestJointModel(unittest.TestCase):
     def test_text_input(self):
         text = "Emory NLP is a research lab in Atlanta, GA. "
         "It is founded by Jinho D. Choi in 2014. Dr. Choi is a professor at Emory University."
-
-        doc = en.parse([Input(text=text)])[0]
-        # print(doc)
+        doc = en_services.parser.parse([Input(text=text)])[0]
+        print(doc)
 
     def test_sents_input(self):
         text = ["Emory NLP is a research lab in Atlanta, GA.",
                 "It is founded by Jinho D. Choi in 2014.",
                 'Dr. Choi is a professor at Emory University.']
-
-        doc = en.parse([Input(text=text)])[0]
-        # print(doc)
+        doc = en_services.parser.parse([Input(text=text)])[0]
+        print(doc)
 
     def test_tokens_input(self):
         tokens = [
@@ -32,8 +30,8 @@ class TestJointModel(unittest.TestCase):
             ["It", "is", "founded", "by", "Jinho", "D.", "Choi", "in", "2014", "."],
             ["Dr.", "Choi", "is", "a", "professor", "at", "Emory", "University", "."]
         ]
-        doc = en.parse([Input(tokens=tokens)])[0]
-        # print(doc)
+        doc = en_services.parser.parse([Input(tokens=tokens)])[0]
+        print(doc)
 
 
 if __name__ == '__main__':
