08c7a5a86da2bb5a3eab25fc64cd6fb6022f2162
lxucs <leonxu624@gmail.com>
2020-12-23 12:23:52 -0500

    Added coref test

--- tests/test_joint_model.py
+++ tests/test_joint_model.py
@@ -10,12 +10,12 @@ class TestTransformerNamedEntityRecognizer(unittest.TestCase):
         super().setUp()
 
     def test_joint_model(self):
-        from elit.server.en import en
+        from elit.server.en import en_services
         from elit.server.format import Input
         text = "Emory NLP is a research lab in Atlanta, GA. "
         "It is founded by Jinho D. Choi in 2014. Dr. Choi is a professor at Emory University."
 
-        doc = en.parse([Input(text=text)])[0]
+        doc = en_services.parser.parse([Input(text=text)])[0]
         print(doc)
 
 
---
+++ tests/test_service_coref_online.py
@@ -0,0 +1,45 @@
+# ========================================================================
+# Copyright 2020 Emory University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+
+# -*- coding:utf-8 -*-
+# Author: Liyan Xu
+import unittest
+
+
+class TestOnlineCoref(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_online_coref(self):
+        from elit.server.en import en_services
+        from elit.server.format import Input
+        text = 'Pfizer said last week it may need the U.S. government to help it secure some components needed to ' \
+               'make the vaccine. While the company halved its 2020 production target due to manufacturing issues, ' \
+               'it said last week its manufacturing is running smoothly now. The government also has the option to ' \
+               'acquire up to an additional 400 million doses of the vaccine.'
+        inputs = [Input()]
+        for input_doc in inputs:
+            input_doc.text = text[:]
+            input_doc.models = ['ocr']
+
+        docs = en_services.online_coref.predict(inputs)
+        assert len(docs) == len(inputs)
+        print(docs[-1])
+
+
+if __name__ == '__main__':
+    unittest.main()
