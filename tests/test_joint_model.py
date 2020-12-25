b50b37770496226f6650e991121e22b22209df83
hankcs <jfservice@126.com>
2020-12-25 12:45:01 -0500

    Unittest for 3 formats of inputs. Push codes with confidence.

--- tests/test_joint_model.py
+++ tests/test_joint_model.py
@@ -2,20 +2,36 @@
 # Author: hankcs
 # Date: 2020-12-18 19:21
 import unittest
+from elit.server.en import en_services
+from elit.server.format import Input
 
 
-class TestTransformerNamedEntityRecognizer(unittest.TestCase):
+class TestJointModel(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
 
-    def test_joint_model(self):
-        from elit.server.en import en_services
-        from elit.server.format import Input
+    def test_text_input(self):
         text = "Emory NLP is a research lab in Atlanta, GA. "
         "It is founded by Jinho D. Choi in 2014. Dr. Choi is a professor at Emory University."
 
         doc = en_services.parser.parse([Input(text=text)])[0]
+        print(doc)
+
+    def test_sents_input(self):
+        text = ["Emory NLP is a research lab in Atlanta, GA. ",
+                "It is founded by Jinho D. Choi in 2014. Dr. Choi is a professor at Emory University."]
+
+        doc = en_services.parser.parse([Input(text=text)])[0]
+        print(doc)
+
+    def test_tokens_input(self):
+        tokens = [
+            ["Emory", "NLP", "is", "a", "research", "lab", "in", "Atlanta", ",", "GA", "."],
+            ["It", "is", "founded", "by", "Jinho", "D.", "Choi", "in", "2014", ".", "Dr.", "Choi", "is", "a",
+             "professor", "at", "Emory", "University", "."]
+        ]
+        doc = en_services.parser.parse([Input(tokens=tokens)])[0]
         print(doc)
 
 
