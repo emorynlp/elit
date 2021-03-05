155be3dd5c65ddc91a655ee54423e944095d4281
hankcs <jfservice@126.com>
2021-03-05 01:24:10 -0500

    Improve robustness on erroneous con tree

--- elit/common/document.py
+++ elit/common/document.py
@@ -34,7 +34,7 @@ def tree_to_list(T):
 def list_to_tree(L):
     if isinstance(L, str):
         return L
-    while len(L) == 1:
+    while len(L) == 1 and isinstance(L, list):
         L = L[0]
     return Tree(L[0], [list_to_tree(child) for child in L[1]])
 
