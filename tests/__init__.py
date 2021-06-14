f50740958e05f79d78ec25e86dcb9b4f320682a4
hankcs <jfservice@126.com>
2021-06-14 15:20:27 -0400

    Add a helper function to test module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding:utf-8 -*-
 # Author: hankcs
 # Date: 2020-12-18 19:21
+import os
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+
+def cdroot():
+    """
+    cd to project root, so models are saved in the root folder
+    """
+    os.chdir(root)
